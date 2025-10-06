38eed81bf991eb702483d3bd7ab133b62e6d3994
Achal Jhawar <achaldps@gmail.com>
2025-10-06 18:15:24 +1100

    refactor: remove unused variables

--- src/env.js
+++ src/env.js
@@ -16,8 +16,6 @@ export const env = createEnv({
     AUTH_DISCORD_ID: z.string(),
     AUTH_DISCORD_SECRET: z.string(),
     SUPABASE_DATABASE_URL: z.string().url(),
-    UPSTASH_REDIS_REST_URL: z.string().url(),
-    UPSTASH_REDIS_REST_TOKEN: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -43,8 +41,6 @@ export const env = createEnv({
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     SUPABASE_DATABASE_URL: process.env.SUPABASE_DATABASE_URL,
-    UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
   },
   /**
